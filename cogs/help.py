b43321105c0a710c31dbe9f95c067e1a559c5090
ankushKun <ankus4singh@gmail.com>
2021-04-17 19:43:32 +0530

    added source

--- cogs/help.py
+++ cogs/help.py
@@ -12,16 +12,25 @@ class Help(commands.Cog):
         try:
 
             h = discord.Embed(
-                title='', description='Need Help?\n<> - optional arguments\n() - needed arguments\n [] - aliases', color=0x006900)
-            h.add_field(name='__ABOUT__', value=f"\nPrefix : `S.`")
-            h.add_field(name='__STUDY VC__',
-                        value=f"`studytime <@user>` [st]\n`leaderboard <d/w/m>` [lb] | d-daily, w-weekly, m-monthly")
+                title="",
+                description="Need Help?\n<> - optional arguments\n() - needed arguments\n [] - aliases",
+                color=0x006900,
+            )
+            h.add_field(name="__ABOUT__", value=f"\nPrefix : `S.`")
+            h.add_field(
+                name="__STUDY VC__",
+                value=f"`studytime <@user>` [st]\n`leaderboard <d/w/m>` [lb] | d-daily, w-weekly, m-monthly",
+            )
 
             await ctx.send(embed=h)
         except Exception as e:
             print(e)
 
+    @commands.command()
+    async def source(self, ctx):
+        await ctx.send("https://github.com/o-BigBoy/SankhoCordBot")
+
 
 def setup(bot):
     bot.add_cog(Help(bot))
-    print('---> HELP LOADED')
+    print("---> HELP LOADED")
